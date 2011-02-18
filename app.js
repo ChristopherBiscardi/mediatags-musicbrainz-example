ca7e20c85e8e7c14caddd68e060f9c9d89e4e936
Christopher Biscardi <biscarch@sketcht.com>
2011-02-18 11:42:40 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var querystring = require('querystring');
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyDecoder());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.staticProvider(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+function getArtist(artistName){
+var options = {
+  host: 'www.musicbrainz.org',
+  port: 80,
+  path: '/ws/1/artist/?type=xml&name='+querystring.stringify(artistName)
+};
+
+console.log(options);
+
+http.get(options, function(res) {
+  console.log("Got response: " + res.statusCode);
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
+}
+
+// Routes
+
+app.get('/', function(req, res){
+getArtist('Jaco Pastorius');
+/*
+  res.render('index', {
+      title: 'artist'
+  });*/
+});
+
+// Only listen on $ node app.js
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log("Express server listening on port %d", app.address().port)
+}
